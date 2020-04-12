d9375e9354d2bd94182f455710af5646671426a2
sarahshy <sarahshy@gmail.com>
2020-04-12 14:31:26 -0400

    added perturbing function

--- perturb_predict.R
+++ perturb_predict.R
@@ -21,3 +21,28 @@ print("finished loading")
 # Useful functions
 ################################################
 
+##### The lines below create one full perturbed set
+# can alternatively (and more concisely) just sum measurement errors,
+# but this way we can keep the perturbed magnitudes
+
+# prep column names
+mag.names <- c("u_adj", "g_adj", "r_adj", "i_adj", "z_adj")
+err.names <- c("psfMagErr_u", "psfMagErr_g", "psfMagErr_r", "psfMagErr_i", "psfMagErr_z")
+
+# pull only necessary columns from trains and test sets
+train.mags <- train.set %>% select(all_of(mag.names))
+train.errors <- train.set %>% select(all_of(err.names))
+test.mags <- test.set %>% select(all_of(mag.names))
+test.errors <- test.set %>% select(all_of(err.names))
+
+# perturb
+create.perturbed.dat <- function(mags.df, errors.df){
+  mapply(FUN = function(mag, error, i, j){ mag + rnorm(1, 0, error) },
+         mags.df,
+         errors.df) %>%
+    as.data.frame
+}
+
+
+
+
